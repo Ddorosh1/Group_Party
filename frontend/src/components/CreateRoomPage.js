9d256cd45ded331dedba4068e5c53cc5c38987e3
Ddorosh1 <doroshdmitriy@gmail.com>
2020-11-18 22:34:25 -0800

    Adding Create Room functionality on backend

---
+++ frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,11 @@
+import React, { Component, component } from 'react';
+
+export default class CreateRoomPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <p>This is the create a room page</p>;
+    }
+}
